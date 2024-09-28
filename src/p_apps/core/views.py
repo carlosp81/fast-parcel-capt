48dfc49873745583956822ebc7d057bcce961f78
carlos_APT <capimentel64@gmail.com>
2024-09-28 07:11:37 -0500

    Refactor templates to extends from base.html
    With this refactor the navigation bar it is going to be share for all
    templateswhich could be extend from base.html, including customer and
    courier pages.

--- src/p_apps/core/views.py
+++ src/p_apps/core/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "core/index.html")
+
+def customer_page(request):
+    return render(request, "core/customer.html")
+
+def courier_page(request):
+    return render(request, "core/courier.html")
